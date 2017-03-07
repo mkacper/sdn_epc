b80827ebda4b0b12318fafa62da33ca36f3668cf
Kacper Mentel <kmenteladm@gmail.com>
2017-03-07 11:58:17 +0100

    fix mixfile and ofsh callback module

--- lib/sdn_epc/ofs_calls.ex
+++ lib/sdn_epc/ofs_calls.ex
@@ -1,7 +1,6 @@
 defmodule SdnEpc.OfshCalls do
 
   def init(_mode, _ip, datapatch_id, _features, _version, _connection, _opts) do
-    response = {:ok, datapatch_id}
-    inspect response
+    {:ok, datapatch_id}
   end
 end

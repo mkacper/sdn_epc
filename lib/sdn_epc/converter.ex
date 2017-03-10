d0d9bc5a301c7f8c1637cf05834fa560b709bea9
Kacper Mentel <mentel.kk@gmail.com>
2017-03-10 22:35:58 +0100

    handle particular records from ofp; start creating converter module

---
+++ lib/sdn_epc/converter.ex
@@ -0,0 +1,8 @@
+defmodule SdnEpc.Converter do
+
+  def to_ofp_message_record({:packet_in, xid, [
+                                :buffer_id = buffer_id,
+                                :reason = 
+                              ]}) do
+  end
+end

1466e82c68437cb9083aafae8028d2902be91696
Kacper Mentel <mentel.kk@gmail.com>
2017-03-12 23:56:52 +0100

    fix code style issues

--- lib/sdn_epc/forwarder.ex
+++ lib/sdn_epc/forwarder.ex
@@ -9,18 +9,18 @@ defmodule SdnEpc.Forwarder do
 
   def subscribe_messages_from_switch(datapatch_id, type) do
     GenServer.cast __MODULE__,
-    {:handle_switch_msg, datapatch_id, type}
+      {:handle_switch_msg, datapatch_id, type}
   end
 
   def open_ofp_channel(sup, switch_id, ip, port, version) do
     GenServer.call __MODULE__,
-    {:open_of_channel, %{sup: sup, switch_id: switch_id, ip: ip,
+      {:open_of_channel, %{sup: sup, switch_id: switch_id, ip: ip,
                          port: port, version: version}}
   end
 
   def send_msg_to_switch(datapatch_id, msg) do
     GenServer.cast __MODULE__,
-    {:send_msg_to_switch, datapatch_id: datapatch_id, msg: msg}
+      {:send_msg_to_switch, datapatch_id: datapatch_id, msg: msg}
   end
 
   def send_msg_to_controller(switch_id, msg) do
